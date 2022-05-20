315f040ae18be5528baf94977b4bf585bce735b5
yingtongmiao <105888068+yingtongmiao@users.noreply.github.com>
2022-05-20 15:28:04 +0800

    Update test_atomicwriting.py

--- test_atomicwriting.py
+++ test_atomicwriting.py
@@ -33,12 +33,14 @@ class TestAtomicwriting(unittest.TestCase):
     def test_null_1(self):
         res = atomicwriting("writingfile.txt", "")
         self.assertEqual(res, "Writing contents are empty.")
+        
+#     File path is incorrect. Current path does not have "file" folder.
+#     def test_filename_0(self):
+#         res = atomicwriting("file\writingfile.txt", "Yingtong Miao\nTammy")
+#         self.assertEqual(res, "File name or file path error")
 
-    def test_filename_0(self):
-        res = atomicwriting("file\writingfile.txt", "Yingtong Miao\nTammy")
-        self.assertEqual(res, "File name or file path error")
-
-    def test_filename_1(self):
-        res = atomicwriting(r"C:\Users\Lenovo\Desktop\a", "Yingtong Miao\nTammy")
-        self.assertEqual(res, "File name or file path error")
+#     "a"  is a folder.
+#     def test_filename_1(self):
+#         res = atomicwriting(r"C:\Users\Lenovo\Desktop\a", "Yingtong Miao\nTammy")
+#         self.assertEqual(res, "File name or file path error")
 
